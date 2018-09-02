702dcd2a060a2768e63cbbf36c1394204cb57e2a
Deseteral <deseteral@gmail.com>
2018-09-02 17:09:55 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function createOnActionMiddleware() {
+  const subscriptions = {};
+
+  const middleware = ({ dispatch, getState }) => next => (action) => {
+    if (!subscriptions[action.type]) return next(action);
+    subscriptions[action.type].forEach(callback => callback(action, dispatch, getState));
+    return getState();
+  };
+
+  const onAction = (action, callback) => {
+    if (!subscriptions[action]) subscriptions[action] = [];
+    subscriptions[action] = callback;
+  };
+
+  return { middleware, onAction };
+}
+
+export default createOnActionMiddleware;
